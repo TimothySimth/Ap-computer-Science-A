c944f6d3eab7d5874a9564e9e82cb4f68b424fad
TimothySimth <smith.t7@stu.janesville.k12.wi.us>
2024-09-10 11:27:10 -0500

    9/10

--- src/Q1/prog58h.java
+++ src/Q1/prog58h.java
@@ -1,7 +1,27 @@
 package Q1;
 
+import java.util.Scanner;
+
 public class prog58h {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Please enter KWH used: ");
+        double p = input.nextDouble();
+
+        System.out.print("Please enter KWH used: ");
+        double r = input.nextDouble();
+
+        System.out.print("Please enter KWH used: ");
+        double n = input.nextDouble();
+
+        System.out.print("Please enter KWH used: ");
+        double t = input.nextDouble();
+
+        double a = p * (1+ Math.pow(((n*t)/365), ((0.01*r)/n)));
+
+        System.out.println(a);
+
 
     }
 }
--- src/Q1/prog93a.java
+++ src/Q1/prog93a.java
@@ -17,9 +17,28 @@ public class prog93a {
         double roundtax = Math.round(tax * 100.0) / 100.0;
         double fin = chr + sur + tax;
         double roundfin = Math.round(fin * 100.0) / 100.0;
-        double ;
+        double latefee = fin * 0.04;
+        double roundlatefee = Math.round(latefee * 100.0) / 100.0;
+        double pow = roundfin + roundlatefee;
 
-        System.out.println("  COPMSIC Electric\n--------------------------------\nKilowatts USED\t\t     " + num1 + "\n--------------------------------\n\nBase rate " + num1 + " @0.0475  $" + roundchr + "\nSurcharge\t\t\t\t  $" + roundsur + "\nCitytax\t\t\t\t      $" + roundtax + "\n\t\t\t\t\t    _______\nPay This Amount           $" + roundfin + "\n\nAfter May 20th         $" +roundexfin);
+
+        System.out.println("  COPMSIC Electric\n--------------------------------\nKilowatts USED\t\t     " + num1 + "\n--------------------------------\n\nBase rate " + num1 + " @0.0475  $" + roundchr + "\nSurcharge\t\t\t\t  $" + roundsur + "\nCitytax\t\t\t\t      $" + roundtax + "\n\t\t\t\t\t    _______\nPay This Amount           $" + roundfin + "\n\nAfter May 20th            $" + pow);
 
     }
 }
+/*
+Please enter KWH used: 993
+
+  COPMSIC Electric
+--------------------------------
+Kilowatts USED		     993.0
+--------------------------------
+
+Base rate 993.0 @0.0475  $47.17
+Surcharge				  $4.72
+Citytax				      $1.42
+					    _______
+Pay This Amount           $53.3
+
+After May 20th            $55.43
+*/
