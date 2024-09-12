4ff93bd3a8344a1b3c614265507a261ddc0d6744
TimothySimth <smith.t7@stu.janesville.k12.wi.us>
2024-09-12 11:25:16 -0500

    9/12

--- src/Q1/prog58h.java
+++ src/Q1/prog58h.java
@@ -1,28 +1,38 @@
 package Q1;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class prog58h {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        System.out.print("Please enter KWH used: ");
+        System.out.print("Amount Saved: ");
         double p = input.nextDouble();
 
-        System.out.print("Please enter KWH used: ");
+        System.out.print("Interest rate? ");
         double r = input.nextDouble();
 
-        System.out.print("Please enter KWH used: ");
+        System.out.print("Number of times compounded per year? ");
         double n = input.nextDouble();
 
-        System.out.print("Please enter KWH used: ");
+        System.out.print("Number of days at interest? ");
         double t = input.nextDouble();
 
-        double a = p * (1+ Math.pow(((n*t)/365), ((0.01*r)/n)));
-        double ap = a + p;
 
-        System.out.println(a);
-        System.out.println(ap);
+        double a = p * Math.pow(1+((0.01*r)/n), ((n*t)/365));
+        double it = a - p;
+
+        System.out.printf("Total interest is: $%.2f\n", it);
+        System.out.printf("Total saved is: $%.2f\n", a);
+
 
     }
 }
+/*
+Amount Saved: 5000
+Interest rate? 11.5
+Number of times compounded per year? 365
+Number of days at interest? 900
+Total interest is: $1638.96
+Total saved is: $6638.96
+*/
---
+++ src/Q1/prog58t.java
@@ -0,0 +1,31 @@
+package Q1;
+
+import java.util.*;
+
+public class prog58t {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter amount: ");
+        double a = input.nextDouble();
+
+        System.out.print("Enter money given: ");
+        double g = input.nextDouble();
+
+        double pennie = 0.01;
+        double nickle = 0.05;
+        double dime   = 0.10;
+        double quarter= 0.25;
+        double dollar = 1;
+
+        double ch = g - a;
+        System.out.println("change due: " + ch);
+
+        double nodo = ;
+
+
+
+
+
+    }
+}
