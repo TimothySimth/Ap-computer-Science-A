f8f70091cac8018b4b579afdde747fef59150888
TimothySimth <143823817+TimothySimth@users.noreply.github.com>
2024-12-19 12:51:36 -0600

    12/19

--- src/Q2/Prog410t/prog410t.java
+++ src/Q2/Prog410t/prog410t.java
@@ -1,4 +1,21 @@
 package Q2.Prog410t;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class prog410t {
+    public static void main(String[] args) {
+        try {
+            Scanner file = new Scanner(new File("Langdat/survey.dat"));
+            Scanner file2 = new Scanner(new File("Langdat/survey2.dat"));
+
+            while (file.hasNext()) {
+
+
+            }
+        } catch (IOException e) {
+            System.out.println("Error: " + e);
+        }
+    }
 }
--- src/Q2/Prog410t/prog410tClass.java
+++ src/Q2/Prog410t/prog410tClass.java
@@ -1,4 +1,24 @@
 package Q2.Prog410t;
 
 public class prog410tClass {
+    private int myId;
+    private int myIncome;
+    private int myMembers;
+    private double myPoverty;
+
+    public prog410tClass(int id, int income, int members) {
+        myId      = id;
+        myIncome  = income;
+        myMembers = members;
+
+    }
+
+    public void calc(){
+        myPoverty = 3750.00 + 750.00 * (myMembers-2);
+        if (myIncome >= myPoverty){
+
+        }
+    }
+
+
 }
