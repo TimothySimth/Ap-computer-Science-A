859fcde722a9ece8d16abb85a8a908752e4432a1
Timothy Smith <smith.t7@stu.janesvill.k12.wi.us>
2025-03-21 13:46:49 -0500

    3/14

---
+++ src/Q3/MSOE/MSOE_2021_1.java
@@ -0,0 +1,14 @@
+package Q3.MSOE;
+
+import java.util.Scanner;
+
+public class MSOE_2021_1 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Letter you want found: ");
+        String l = input.next();
+        System.out.print("Enter the sentence: ");
+        String sen = input.next();
+
+    }
+}
--- src/Q3/MSOE/MSOE_2022_5.java
+++ src/Q3/MSOE/MSOE_2022_5.java
@@ -1,5 +1,10 @@
 package Q3.MSOE;
 
+import Q3.Prog505a.BookPerson;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class MSOE_2022_5 {
@@ -7,14 +12,36 @@ public class MSOE_2022_5 {
         Scanner input = new Scanner(System.in);
         System.out.print("How Many Inputs: ");
         int n = input.nextInt();
-        System.out.println("Enter seashells: ");
-        double buckets = 0.0;
+        System.out.print("Enter seashells: ");
+        List<shells> list = new ArrayList<>();
+        int num = input.nextInt();
+        shells shell = new shells(num);
+        list.add(shell);
 
-        for (int i = 0; i < n; i++){
-            buckets+= (double) input.nextInt()/100;
-        }
 
 
-        System.out.printf("You need %.0f bucket for this order", buckets);
+
+
+        System.out.println(list.size());
     }
+}
+
+
+
+
+class shells {
+
+    int myBuketWeight;
+    final int MAX = 100;
+
+
+    public shells(int buketWeight){
+        myBuketWeight = buketWeight;
+    }
+
+    public int getBuketWeight() { return myBuketWeight; }
+
+    public void setBuketWeight(int buketWeight) { myBuketWeight = buketWeight; }
+
+
 }
